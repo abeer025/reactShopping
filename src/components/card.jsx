8be402fcb5f7a18035131b2aa24cf9c37c9372fc
abeer025 <abeeriqbalahmed@gmail.com>
2024-09-08 11:09:52 -0700

    done

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 
-function Card(item ) {
-  const [ thumbnail, category, title, price, id ] = item;
+function Card({ item }) {
+  const { thumbnail, category, title, price, id } = item; // Object destructuring
 
   return (
     <Link
-      to={`/Products/${id}`}
+      to={`/products/${id}`}
       className="lg:w-1/4 md:w-1/2 p-4 w-full shadow"
     >
       <div>
-        <a className="block relative h-48 rounded overflow-hidden">
+        <div className="block relative h-48 rounded overflow-hidden">
           <img
             alt="ecommerce"
             className="object-cover object-center w-full h-full block"
             src={thumbnail}
           />
-        </a>
+        </div>
         <div className="mt-4">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
             {category}
