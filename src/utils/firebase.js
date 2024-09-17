4fee472a87226208441fe902b3beef0cbdea679e
abeer025 <abeeriqbalahmed@gmail.com>
2024-09-17 05:00:21 -0700

    secret remove

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -18,14 +18,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDmVx3u-obS0JHH5V7tpNWBDsI29gGGvYE",
-  authDomain: "e-commerce-website-428bd.firebaseapp.com",
-  databaseURL: "https://e-commerce-website-428bd-default-rtdb.firebaseio.com",
-  projectId: "e-commerce-website-428bd",
-  storageBucket: "e-commerce-website-428bd.appspot.com",
-  messagingSenderId: "147540178257",
-  appId: "1:147540178257:web:348b59daa378fc316f8f70",
-  measurementId: "G-6R5KRVEB1Z"
+
 }
 
 // Initialize Firebase
