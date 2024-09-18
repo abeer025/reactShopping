251289551a2810ab28eda39bbaa30197c892ffe8
abeer025 <abeeriqbalahmed@gmail.com>
2024-09-18 12:54:50 -0700

    card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Box,Button,Typography } from "@mui/material";
+import { Products} from "../pages/Products";
 
 function Card({ item }) {
   const { thumbnail, Category, title, price, id } = item;
