b0e48c382803a837c9e6d4a601bfb8cb1e516a11
abeer025 <abeeriqbalahmed@gmail.com>
2024-12-09 19:40:52 +0500

    check

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Category from "../components/Categories";
-import Card from "../components/Card";
+import {Card} from "../components/Card";
 import { Select, MenuItem, TextField, Button, CircularProgress } from "@mui/material";
 
 function Products() {
